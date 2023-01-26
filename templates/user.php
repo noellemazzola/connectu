7da72236ae0652761fdcd5a42b27be4caf5810e2
Hanna Bui <hannabui00@gmail.com>
2023-01-25 19:34:56 -0500

    update

---
+++ templates/user.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>User</title>
+    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+<i class="fa fa-chevron-left" style="font-size:36px"></i>
+</body>
+
+</html>
