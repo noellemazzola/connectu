c6743b50c7479fed6ff63760ca5d2188089e9edc
Hanna Bui <hannabui00@gmail.com>
2023-01-25 22:26:27 -0500

    update

--- templates/user.php
+++ templates/user.php
@@ -4,14 +4,26 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>User</title>
-    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
 </head>
 
 <body>
     <div class="back">
         <i class="fa fa-chevron-left" style="font-size:36px"></i>
     </div>
+
+    <div class="userpic">
+      <img src="images/profile/michael-dam-mEZ3PoFGs_k-unsplash.jpg">
+    </div>
+
+    <div class="userprofile">
+        <h2>@kate.zimmer</h2>
+        <h4>Class of 25' | Love to explore</h4>
+        <button onclick="conversation.php">Message</button>
+      </div>
+   
 </body>
 
 </html>
