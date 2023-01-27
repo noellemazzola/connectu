667755c23a9f046b29d2eb476166ac4f1aa64a5f
Hanna Bui <hannabui00@gmail.com>
2023-01-26 22:39:50 -0500

    update

---
+++ profile/clubprofile.php
@@ -0,0 +1,46 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Club Profile</title>
+    <style>
+    <?php include ("/css/main.css")?>
+    </style>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+  <div class="clubicons">
+    <div class="cicon">
+      <i class='fa fa-plus' style='font-size:36px'></i>
+      <i class="fa fa-gear" style='font-size: 36px'></i>
+    </div>
+  </div>
+
+    <div class="userpic">
+      <img src="images/clubs/cab.jpeg">
+    </div>
+
+    <div class="userprofile">
+        <h2>Campus Activities Board</h2>
+        <h4>Our mission at UCF's Campus Activities Board (CAB) is to foster
+            a sense of community on campus by hosting unique events that 
+            effectively cater to our diverse student body, and to the Orlando
+            community.
+        </h4>
+        <ul>
+          <li><i class='fa fa-youtube' style='font-size:36px'></i></li>
+          <li><i class='fa fa-instagram' style='font-size:36px'></i></li>
+          <li><i class='fa fa-twitter' style='font-size:36px'></i></li>
+          <li><i class='fa fa-facebook' style='font-size:36px'></i></li>
+        </ul>
+      </div>
+
+    <div class="clubpost">
+        <h2>Upcoming Events</h2>
+    </div>
+   
+</body>
+
+</html>
