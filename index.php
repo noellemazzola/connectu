2a30efe97605f714f7165fc666e1b4b3e4772d83
Fridjinah <48133253+JinahC@users.noreply.github.com>
2023-01-30 15:44:04 -0500

    added promoted events

--- index.php
+++ index.php
@@ -1,6 +1,62 @@
 <?php include "templates/header.php" ?>
+<main class="p-3">
+    <i class="bi bi-bell"></i>
+    <h2>Promoted Events</h2>
+    <div class="scrolling-wrapper">
+        <div class="scroll-item">
+            <div class="event">
+                <div class="event--grid-item">
+                    <img class="event--img" src="Images/events/ucfvssmu.png"/>
+                </div>
+                <div class="event--info">
+                    <p class="event--title">Men's Basketball Game: UCF vs. SMU</p>
+                    <p class="event--date">Sun, Jan. 8 - 2PM</p>
+                    <p class="event--location">Addition Financial Arena</p>
+                    <div class="event--icons">
+                        <i class="bi bi-person-check-fill"></i>
+                        <i class="bi bi-share"></i>
+                        <i class="bi bi-heart"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="scroll-item">
+            <div class="event">
+                <div class="event--grid-item">
+                    <img class="event--img" src="Images/events/ucfvssmu.png"/>
+                </div>
+                <div class="event--info">
+                    <p class="event--title">Men's Basketball Game: UCF vs. SMU</p>
+                    <p class="event--date">Sun, Jan. 8 - 2PM</p>
+                    <p class="event--location">Addition Financial Arena</p>
+                    <div class="event--icons">
+                        <i class="bi bi-person-check-fill"></i>
+                        <i class="bi bi-share"></i>
+                        <i class="bi bi-heart"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="scroll-item">
+            <div class="event">
+                <div class="event--grid-item">
+                    <img class="event--img" src="Images/events/ucfvssmu.png"/>
+                </div>
+                <div class="event--info">
+                    <p class="event--title">Men's Basketball Game: UCF vs. SMU</p>
+                    <p class="event--date">Sun, Jan. 8 - 2PM</p>
+                    <p class="event--location">Addition Financial Arena</p>
+                    <div class="event--icons">
+                        <i class="bi bi-person-check-fill"></i>
+                        <i class="bi bi-share"></i>
+                        <i class="bi bi-heart"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
-<h2>Promoted Events</h2>
+</main>
 
 
 <?php
