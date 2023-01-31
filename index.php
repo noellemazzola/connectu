708b4477620920d64ba1e250d21c06d129852a47
Fridjinah <48133253+JinahC@users.noreply.github.com>
2023-01-30 20:02:15 -0500

    dashboard finished

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php include "templates/header.php" ?>
 <main class="p-3">
-    <i class="bi bi-bell"></i>
+    <i class="bi bi-bell" style="font-size: 30px; float: right;"></i>
     <h2>Promoted Events</h2>
     <div class="scrolling-wrapper">
         <div class="scroll-item">
@@ -56,6 +56,86 @@
         </div>
     </div>
 
+    <h2>Club Spotlight</h2>
+    <div class="scrolling-wrapper club-spotlight">
+        <div class="scroll-item">
+            <div class="spotlight">
+                <img class="spotlight--img" src="Images/clubs/emo-alt.jpg"/>
+                <p class="spotlight--club-name">Emo Alt Club</p>
+            </div>
+        </div>
+        <div class="scroll-item">
+            <div class="spotlight">
+                <img class="spotlight--img" src="Images/clubs/emo-alt.jpg"/>
+                <p class="spotlight--club-name">Emo Alt Club</p>
+            </div>
+        </div>
+        <div class="scroll-item">
+            <div class="spotlight">
+                <img class="spotlight--img" src="Images/clubs/emo-alt.jpg"/>
+                <p class="spotlight--club-name">Emo Alt Club</p>
+            </div>
+        </div>
+        <div class="scroll-item">
+            <div class="spotlight">
+                <img class="spotlight--img" src="Images/clubs/emo-alt.jpg"/>
+                <p class="spotlight--club-name">Emo Alt Club</p>
+            </div>
+        </div>
+        <div class="scroll-item">
+            <div class="spotlight">
+                <img class="spotlight--img" src="Images/clubs/emo-alt.jpg"/>
+                <p class="spotlight--club-name">Emo Alt Club</p>
+            </div>
+        </div>
+    </div>
+
+    <h2>Upcoming Events</h2>
+    <div class="event">
+        <div class="event--grid-item">
+            <img class="event--img" src="Images/events/ucfvssmu.png"/>
+        </div>
+        <div class="event--info">
+            <p class="event--title">Men's Basketball Game: UCF vs. SMU</p>
+            <p class="event--date">Sun, Jan. 8 - 2PM</p>
+            <p class="event--location">Addition Financial Arena</p>
+            <div class="event--icons">
+                <i class="bi bi-person-check-fill"></i>
+                <i class="bi bi-share"></i>
+                <i class="bi bi-heart"></i>
+            </div>
+        </div>
+    </div>
+    <div class="event">
+        <div class="event--grid-item">
+            <img class="event--img" src="Images/events/ucfvssmu.png"/>
+        </div>
+        <div class="event--info">
+            <p class="event--title">Men's Basketball Game: UCF vs. SMU</p>
+            <p class="event--date">Sun, Jan. 8 - 2PM</p>
+            <p class="event--location">Addition Financial Arena</p>
+            <div class="event--icons">
+                <i class="bi bi-person-check-fill"></i>
+                <i class="bi bi-share"></i>
+                <i class="bi bi-heart"></i>
+            </div>
+        </div>
+    </div>
+    <div class="event">
+        <div class="event--grid-item">
+            <img class="event--img" src="Images/events/ucfvssmu.png"/>
+        </div>
+        <div class="event--info">
+            <p class="event--title">Men's Basketball Game: UCF vs. SMU</p>
+            <p class="event--date">Sun, Jan. 8 - 2PM</p>
+            <p class="event--location">Addition Financial Arena</p>
+            <div class="event--icons">
+                <i class="bi bi-person-check-fill"></i>
+                <i class="bi bi-share"></i>
+                <i class="bi bi-heart"></i>
+            </div>
+        </div>
+    </div>
 </main>
 
 
