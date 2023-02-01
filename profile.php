ab582308e584fa7d7051272ada3c7d304075701a
Hanna Bui <hannabui00@gmail.com>
2023-02-01 14:15:11 -0500

    update

--- profile.php
+++ profile.php
@@ -1,7 +1,7 @@
 <?php include "templates/header.php>"?>
 
     <div class="setgear">
-    <a href="settings.html"><i class='bi bi-gear' style='font-size:36px'></i></a>
+    <a href="settings.php"><i class='bi bi-gear' style='font-size:36px'></i></a>
     </div>
 
     <div class="profilepic">
@@ -11,11 +11,11 @@
     <div class="mainprofile">
         <h2>@isabella.monte</h2>
         <h4>Class of 24' | President of Campus Activities Board</h4>
-        <a href="clubprofile.html">Switch Account</a>
+        <a href="clubprofile.php">Switch Account</a>
       </div>
 
     <div class="friendslistbt">
-      <a href="friendslist.html">Friends List</a>
+      <a href="friendslist.php">Friends List</a>
     </div>
 
 <?php 
