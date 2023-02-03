fa7a9cf3d5e4a20ebd115c0b3c8b92c674e3be7a
Fridjinah <48133253+JinahC@users.noreply.github.com>
2023-02-02 19:39:06 -0500

    attending list done

---
+++ attendinglist.php
@@ -0,0 +1,43 @@
+<?php include "templates/header.php" ?>
+<div class="page-title">
+    <a class="back-arrow" href="dashboard.php"><i class="bi bi-chevron-left"></i></a>
+    <h2>Attending List</h2>
+</div>
+<div class="d-block px-4 noti-group">
+    <div class="py-3 d-flex justify-content-between align-items-center">
+        <img class="noti-group--img" src="Images/profile/michael-dam-mEZ3PoFGs_k-unsplash.jpg">
+        <p class="mb-0">@kate.zimmer</p>
+        <i class="bi bi-chevron-right ml-auto"></i>
+    </div>
+    <div class="py-3 d-flex justify-content-between align-items-center">
+        <img class="noti-group--img" src="Images/profile/christopher-campbell-rDEOVtE7vOs-unsplash.jpg">
+        <p class="mb-0">@natalie.rexar</p>
+        <i class="bi bi-chevron-right ml-auto"></i>
+    </div>
+    <div class="py-3 d-flex justify-content-between align-items-center">
+        <img class="noti-group--img" src="Images/profile/darshan-patel-QJEVpydulGs-unsplash.jpg">
+        <p class="mb-0">@kyle.cao</p>
+        <i class="bi bi-chevron-right ml-auto"></i>
+    </div>
+    <div class="py-3 d-flex justify-content-between align-items-center">
+        <img class="noti-group--img" src="Images/profile/karabo-mdluli-Y7TEMCKRIgI-unsplash.jpg">
+        <p class="mb-0">@jordan.king</p>
+        <i class="bi bi-chevron-right ml-auto"></i>
+    </div>
+    <div class="py-3 d-flex justify-content-between align-items-center">
+        <img class="noti-group--img" src="Images/profile/dillon-kydd-J6MK67MIs0I-unsplash.jpg">
+        <p class="mb-0">@andrea.hernandez</p>
+        <i class="bi bi-chevron-right ml-auto"></i>
+    </div>
+    <div class="py-3 d-flex justify-content-between align-items-center">
+        <img class="noti-group--img" src="Images/profile/alexander-hipp-iEEBWgY_6lA-unsplash.jpg">
+            <p class="mb-0">@brad.krawford</p>
+        <i class="bi bi-chevron-right ml-auto"></i>
+    </div>
+</div>
+<button class="button d-block m-auto">I'm Going!</button>
+
+<?php 
+    include "templates/navbar.php";
+    include "templates/footer.php";
+?>
