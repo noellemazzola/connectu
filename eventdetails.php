932293142ad6ccaed790e58a448cc084db564dca
Fridjinah <48133253+JinahC@users.noreply.github.com>
2023-02-03 00:01:51 -0500

    event page done

--- eventdetails.php
+++ eventdetails.php
@@ -1,12 +1,92 @@
 <?php include "templates/header.php" ?>
 <img class="w-100" src="Images/events/ucfvssmu.png"/>
-<main class="p-2">
+<main class="p-3">
     <div class="d-flex justify-content-between">
         <a class="back-arrow" href="dashboard.php"><i class="bi bi-chevron-left"></i></a>
         <div class="event--icons">
-            <a href="attendinglist.php"><i class="bi bi-person-check-fill"></i></a>
-            <i class="bi bi-share"></i>
-            <i class="bi bi-heart"></i>
+            <a href="attendinglist.php"><i class="bi bi-person-check-fill larger"></i></a>
+            <i class="bi bi-share larger"></i>
+            <i class="bi bi-heart larger"></i>
+        </div>
+    </div>
+    <h3 class="details--title">Men's Basketball Game: UCF vs. SMU</h3>
+    <div class="align-items-center">
+        <i class="bi bi-calendar details--icon"></i>
+        <div class="d-block">
+            <p class="details--info">Sunday, January 8, 2023</p>
+            <p class="details--info">2 PM</p>
+        </div>
+    </div>
+    <div class="align-items-center mt-2">
+        <i class="bi bi-geo-alt details--icon"></i>
+        <p class="details--info">Addition Financial Arena</p>
+    </div>
+    <h4 class="details--details">Details</h4>
+    <p class="details--desc">Come out and support our Knights as we take on the SMU Mustangs! <a href="https://ucfknights.com/sports/2021/2/1/Ticket%20Home%20Page.aspx">Get Tickets</a></p>
+
+    <img class="details--profile-pic mt-4" src="Images/clubs/knights-basketball.png"/>
+    <p class="text-center my-2 details--club-name">Men's Basketball</p>
+    <button class="follow following d-block m-auto">FOLLOWING</button>
+
+    <h3 class="details--more-events mt-5">More Events by Men's Athletics</h3>
+    <div class="event">
+        <div class="event--grid-item">
+            <img class="event--img" src="Images/events/ucfvssmu.png"/>
+        </div>
+        <div class="event--info">
+            <p class="event--title">Men's Basketball Game: UCF vs. SMU</p>
+            <p class="event--date">Sun, Jan. 8 - 2PM</p>
+            <p class="event--location">Addition Financial Arena</p>
+            <div class="event--icons">
+            <a href="attendinglist.php"><i class='bi bi-person-check-fill'></i></a>
+                <i class="bi bi-share"></i>
+                <i class="bi bi-heart"></i>
+            </div>
+        </div>
+    </div>
+    <div class="event">
+        <div class="event--grid-item">
+            <img class="event--img" src="Images/events/ucfvssmu.png"/>
+        </div>
+        <div class="event--info">
+            <p class="event--title">Men's Basketball Game: UCF vs. SMU</p>
+            <p class="event--date">Sun, Jan. 8 - 2PM</p>
+            <p class="event--location">Addition Financial Arena</p>
+            <div class="event--icons">
+            <a href="attendinglist.php"><i class='bi bi-person-check-fill'></i></a>
+                <i class="bi bi-share"></i>
+                <i class="bi bi-heart"></i>
+            </div>
+        </div>
+    </div>
+    <div class="event">
+        <div class="event--grid-item">
+            <img class="event--img" src="Images/events/ucfvssmu.png"/>
+        </div>
+        <div class="event--info">
+            <p class="event--title">Men's Basketball Game: UCF vs. SMU</p>
+            <p class="event--date">Sun, Jan. 8 - 2PM</p>
+            <p class="event--location">Addition Financial Arena</p>
+            <div class="event--icons">
+            <a href="attendinglist.php"><i class='bi bi-person-check-fill'></i></a>
+                <i class="bi bi-share"></i>
+                <i class="bi bi-heart"></i>
+            </div>
+        </div>
+    </div>
+    <div class="event">
+        <div class="event--grid-item">
+            <img class="event--img" src="Images/events/ucfvssmu.png"/>
+        </div>
+        <div class="event--info">
+            <p class="event--title">Men's Basketball Game: UCF vs. SMU</p>
+            <p class="event--date">Sun, Jan. 8 - 2PM</p>
+            <p class="event--location">Addition Financial Arena</p>
+            <div class="event--icons">
+            <a href="attendinglist.php"><i class='bi bi-person-check-fill'></i></a>
+                <i class="bi bi-share"></i>
+                <i class="bi bi-heart"></i>
+            </div>
         </div>
     </div>
 </main>
