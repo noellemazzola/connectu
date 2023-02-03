6585249b3d690481d796ccce0a1a3464dbf3c8cb
Hanna Bui <hannabui00@gmail.com>
2023-02-02 20:39:47 -0500

    update

--- login.php
+++ login.php
@@ -1,7 +1,7 @@
 <?php include "templates/header.php" ?>
 <main class="text-center">
     <img class="logo" src="Images/logo.png" alt="ConnectU's logo; a Capital C with cartoon users inside of the letter">
-    <h1>Welcome to Back to ConnectU!</h1>
+    <h1>Welcome Back to ConnectU!</h1>
     <form>
         <input type="text" placeholder="Username" name="username" id="username" required>
         <input type="password" placeholder="Password" name="psw" id="psw" required>
