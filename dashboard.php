91f1cbef5b688f3da12ac9ba558adca0301ece6b
Fridjinah <48133253+JinahC@users.noreply.github.com>
2023-02-02 20:23:05 -0500

    update

--- dashboard.php
+++ dashboard.php
@@ -5,20 +5,20 @@
     <div class="scrolling-wrapper">
         <div class="scroll-item">
             <div class="event">
-                <div class="event--grid-item">
-                    <img class="event--img" src="Images/events/ucfvssmu.png"/>
-                </div>
-                <div class="event--info">
-                    <p class="event--title">Men's Basketball Game: UCF vs. SMU</p>
-                    <p class="event--date">Sun, Jan. 8 - 2PM</p>
-                    <p class="event--location">Addition Financial Arena</p>
-                    <div class="event--icons">
-                        <a href="attendinglist.php"><i class="bi bi-person-check-fill"></i></a>
-                        <i class="bi bi-share"></i>
-                        <i class="bi bi-heart"></i>
+                    <div class="event--grid-item">
+                        <img class="event--img" src="Images/events/ucfvssmu.png"/>
+                    </div>
+                    <div class="event--info">
+                        <a class="event--title" href="eventdetails.php">Men's Basketball Game: UCF vs. SMU</a>
+                        <p class="event--date">Sun, Jan. 8 - 2PM</p>
+                        <p class="event--location">Addition Financial Arena</p>
+                        <div class="event--icons">
+                            <a href="attendinglist.php"><i class="bi bi-person-check-fill"></i></a>
+                            <i class="bi bi-share"></i>
+                            <i class="bi bi-heart"></i>
+                        </div>
                     </div>
                 </div>
-            </div>
         </div>
         <div class="scroll-item">
             <div class="event">
