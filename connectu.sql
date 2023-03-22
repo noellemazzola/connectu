959679d845282d43660f3c3bf68f8d8b91031aa3
Hanna Bui <hannabui00@gmail.com>
2023-03-22 14:32:34 -0400

    added connectu.

--- connectu.sql
+++ connectu.sql
@@ -27,7 +27,10 @@ SET time_zone = "+00:00";
 -- Table structure for table `clubs`
 --
 
-CREATE TABLE `clubs` (
+CREATE DATEBASE connectu;
+
+CREATE DATEBASE connectu;
+CREATE TABLE connectu.clubs (
   `id` int(11) NOT NULL,
   `club_name` varchar(255) NOT NULL,
   `club_president` varchar(255) NOT NULL,
@@ -45,7 +48,7 @@ CREATE TABLE `clubs` (
 -- Dumping data for table `clubs`
 --
 
-INSERT INTO `clubs` (`id`, `club_name`, `club_president`, `club_email`, `club_phone`, `club_username`, `club_password`, `has_spotlight`, `club_profile_img`, `club_profile_bio`, `club_tags`) VALUES
+INSERT INTO connectu.clubs (`id`, `club_name`, `club_president`, `club_email`, `club_phone`, `club_username`, `club_password`, `has_spotlight`, `club_profile_img`, `club_profile_bio`, `club_tags`) VALUES
 (1, 'Campus Activities Board', 'Nick Maguire', 'cab@ucf.edu', '407-555-5555', 'cabby', 'cab4ever', b'1', 'Images/clubs/cab.jpeg', 'Our mission at UCF\'s Campus Activities Board (CAB) is to foster a sense of community on campus by hosting unique events that effectively cater to our diverse student body and to the Orlando community.', 'social, entertainment'),
 (2, 'Men\'s Basketball', 'Aaron Carter', 'mbb@ucf.edu', '407-888-8888', 'mbb_champs', 'mbb1963', b'0', 'Images/clubs/knights-basketball.png', 'UCF features world-class athletic programs and facilities that are home to one of the biggest and most energetic fan bases in the country, Knight Nation. \r\n\r\nFor schedules, tickets, stats, and more, visit <a href=\"https://ucfknights.com/\">UCFKnights.com</a>. \r\n\r\n#ChargeOn', 'sports'),
 (3, 'Emo/Alt Club', 'Jorge Lopez', 'president@emoalt.org', '407-330-4170', 'emodreamer', 'soin2u', b'1', 'Images/clubs/emo-alt.jpg', 'Emo/Alt club is a club focused on the appreciation of alternative music and culture with a focus on emo subculture. Everyone is welcome to join regardless of their knowledge or prior experiences with these genres! We provide a nonjudgemental space where students can exchange opinions and ideas while having sense of belonging. Students will be provided with opportunities for personal development through leadership opportunities, social events that foster friendships, volunteering events that develop altruism, and study groups that positively contribute to UCF’s student retention. EMO will seek to develop and nourish the alternative community within UCF and the surrounding area.\r\n\r\n<br><br>- Come out and meet other people with similar interests as you or learn about a new style of music!\r\n\r\n<br><br>- Our general meetings are on Thursdays at 6pm with other events being posted regularly.', 'social, music');
@@ -56,7 +59,7 @@ INSERT INTO `clubs` (`id`, `club_name`, `club_president`, `club_email`, `club_ph
 -- Table structure for table `events`
 --
 
-CREATE TABLE `events` (
+CREATE TABLE connectu.events (
   `id` int(11) NOT NULL,
   `event_name` varchar(255) NOT NULL,
   `event_start_date` varchar(255) NOT NULL,
@@ -75,7 +78,7 @@ CREATE TABLE `events` (
 -- Dumping data for table `events`
 --
 
-INSERT INTO `events` (`id`, `event_name`, `event_start_date`, `event_start_time`, `event_end_date`, `event_end_time`, `event_location`, `event_desc`, `event_img`, `event_tags`, `event_organization`, `is_promoted`) VALUES
+INSERT INTO connectu.events (`id`, `event_name`, `event_start_date`, `event_start_time`, `event_end_date`, `event_end_time`, `event_location`, `event_desc`, `event_img`, `event_tags`, `event_organization`, `is_promoted`) VALUES
 (1, 'UCFest Movie Knight', 'Mon, Mar. 20', '8 PM', 'Mon, Mar. 20', '11 PM', 'Addition Financial Arena', '💥 Come kick-off our INCREDIBLE UCFest week with our showing of Marvel Studios’ Thor: Love & Thunder! Doors will open at 7PM and the movie will begin at 8PM. This event is only open to UCF Students, who get in free with a valid UCFID. We look forward to seeing you all there to kick off our MARVEL-ous UCFest week! 💥', 'Images/events/ucfest.png', '', 'Campus Activities Board', b'1'),
 (2, 'Men\'s Basketball Game: UCF vs SMU', 'Sun, Jan 8', '2 PM', 'Sun, Jan 8', '', 'Addition Financial Arena', 'Come out and support our Knights as we take on the SMU Mustangs! <a href=\"https://ucfknights.com/sports/2021/2/1/Ticket%20Home%20Page.aspx\">Get Tickets!</a>', 'Images/events/ucfvssmu.png', '', 'Men\'s Basketball', b'0'),
 (3, 'Emo/Alt GBM', 'Thu, Feb 16', '6 PM', 'Thu, Feb 16', '7 PM', 'BA1 215', 'We welcome everyone regardless of their knowledge of emo/alt genres and cultures.', 'Images/clubs/emo-alt.jpg', 'music, social', 'Emo/Alt Club', b'0');
@@ -86,7 +89,7 @@ INSERT INTO `events` (`id`, `event_name`, `event_start_date`, `event_start_time`
 -- Table structure for table `students`
 --
 
-CREATE TABLE `students` (
+CREATE TABLE connectu.students (
   `id` int(11) NOT NULL,
   `student_fname` varchar(255) NOT NULL,
   `student_lname` varchar(255) NOT NULL,
@@ -104,7 +107,7 @@ CREATE TABLE `students` (
 -- Dumping data for table `students`
 --
 
-INSERT INTO `students` (`id`, `student_fname`, `student_lname`, `student_gradyear`, `student_email`, `student_username`, `student_password`, `student_desc`, `student_profile_img`, `student_favorite_clubs`, `student_favorite_events`) VALUES
+INSERT INTO connectu.students (`id`, `student_fname`, `student_lname`, `student_gradyear`, `student_email`, `student_username`, `student_password`, `student_desc`, `student_profile_img`, `student_favorite_clubs`, `student_favorite_events`) VALUES
 (1, 'Isabella', 'Monte', 'Fall 2024', 'isabella.monte@knights.ucf.edu', 'bellonte', 'og1234', 'Class of 24\' | President of Campus Activities Board', 'Images/profile/alex-starnes-WYE2UhXsU1Y-unsplash.jpg', 'Campus Activities Board, Men\\\'s Basketball', 'Men\\\'s Basketball Game: UCF vs SMU, UCFest Movie Knight'),
 (13, 'uniqua', 'backyardigan', 'spring 2038', 'uniqua@email.com', 'unique', '1234', '', '', '', '');
 
@@ -115,25 +118,25 @@ INSERT INTO `students` (`id`, `student_fname`, `student_lname`, `student_gradyea
 --
 -- Indexes for table `clubs`
 --
-ALTER TABLE `clubs`
+ALTER TABLE connectu.clubs
   ADD PRIMARY KEY (`id`);
-ALTER TABLE `clubs` ADD FULLTEXT KEY `club_tags` (`club_tags`);
-ALTER TABLE `clubs` ADD FULLTEXT KEY `club_name` (`club_name`);
+ALTER TABLE connectu.clubs ADD FULLTEXT KEY `club_tags` (`club_tags`);
+ALTER TABLE connectu.clubs ADD FULLTEXT KEY `club_name` (`club_name`);
 
 --
 -- Indexes for table `events`
 --
-ALTER TABLE `events`
+ALTER TABLE connectu.events
   ADD PRIMARY KEY (`id`);
-ALTER TABLE `events` ADD FULLTEXT KEY `event_tags` (`event_tags`);
-ALTER TABLE `events` ADD FULLTEXT KEY `event_name` (`event_name`);
+ALTER TABLE connectu.events ADD FULLTEXT KEY `event_tags` (`event_tags`);
+ALTER TABLE connectu.events ADD FULLTEXT KEY `event_name` (`event_name`);
 
 --
 -- Indexes for table `students`
 --
-ALTER TABLE `students`
+ALTER TABLE connectu.students
   ADD PRIMARY KEY (`id`);
-ALTER TABLE `students` ADD FULLTEXT KEY `student_favorite_clubs` (`student_favorite_clubs`);
+ALTER TABLE connectu.students ADD FULLTEXT KEY `student_favorite_clubs` (`student_favorite_clubs`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -142,19 +145,19 @@ ALTER TABLE `students` ADD FULLTEXT KEY `student_favorite_clubs` (`student_favor
 --
 -- AUTO_INCREMENT for table `clubs`
 --
-ALTER TABLE `clubs`
+ALTER TABLE connectu.clubs
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `events`
 --
-ALTER TABLE `events`
+ALTER TABLE connectu.events
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `students`
 --
-ALTER TABLE `students`
+ALTER TABLE connectu.students
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 COMMIT;
 
